526b4adc3f44d20756db2597311a9f850fd0d892
Danielwhyte <danwhy@gmail.com>
2018-11-30 10:27:08 +0000

    extracts shared elm elements, https://github.com/club-soda/club-soda-guide/pull/185#pullrequestreview-180188216

--- assets/js/search.js
+++ assets/js/search.js
@@ -2,14 +2,14 @@ var search = document.getElementById('search');
 if (search) {
   var drink_type = window.location.search.slice(1).split("=")[1].replace(/_/g, " ") || "default";
   var searchApp;
-  searchApp = Elm.Search.init({
-    node: search, flags: Object.assign({}, {dtype_filter : drink_type})
+  searchApp = Elm.SearchDrink.init({
+    node: search, flags: Object.assign({}, { dtype_filter: drink_type })
   })
 }
 
 var searchVenue = document.getElementById('search-venue');
 if (searchVenue) {
   Elm.SearchVenue.init({
-    node: searchVenue, flags: {venues: venues}
+    node: searchVenue, flags: { venues: venues }
   })
 }
