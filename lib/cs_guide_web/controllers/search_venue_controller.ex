0aae4d53a2d531d73489c65fd31349bf877df209
Cleop <cleo.pearson@hotmail.co.uk>
2018-12-05 16:03:26 +0000

    #182 Removes external image code for venues

--- lib/cs_guide_web/controllers/search_venue_controller.ex
+++ lib/cs_guide_web/controllers/search_venue_controller.ex
@@ -4,11 +4,12 @@ defmodule CsGuideWeb.SearchVenueController do
   alias CsGuide.Resources.Venue
 
   def index(conn, _params) do
-    venues = Venue.all()
-    |> Venue.preload([:venue_types, :venue_images])
-    |> Enum.sort_by(& &1.venue_name)
+    venues =
+      Venue.all()
+      |> Venue.preload([:venue_types, :venue_images])
+      |> Enum.sort_by(& &1.venue_name)
 
-    cards = Enum.map(venues, fn(v) -> get_venue_card(v) end)
+    cards = Enum.map(venues, fn v -> get_venue_card(v) end)
     render(conn, "index.html", venues: cards)
   end
 
@@ -16,19 +17,17 @@ defmodule CsGuideWeb.SearchVenueController do
     %{
       id: venue.entry_id,
       name: venue.venue_name,
-      types: Enum.map(venue.venue_types, fn(v) -> v.name end),
+      types: Enum.map(venue.venue_types, fn v -> v.name end),
       postcode: venue.postcode,
       cs_score: venue.cs_score,
       image:
         if img = List.first(venue.venue_images) do
-          "https://s3-eu-west-1.amazonaws.com/#{Application.get_env(:ex_aws, :bucket)}/#{img.entry_id}"
+          "https://s3-eu-west-1.amazonaws.com/#{Application.get_env(:ex_aws, :bucket)}/#{
+            img.entry_id
+          }"
         else
-           if venue.external_image do
-              venue.external_image
-            else
-              ""
-            end
-      end
+          ""
+        end
     }
   end
 end
