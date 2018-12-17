1c3da4e782460a7514c360072832be9b3124858f
Robert Francis <robertefrancis18@gmail.com>
2018-12-17 10:59:49 +0000

    create function to hyphenate drink names for url #222

--- assets/elm/DrinkCard.elm
+++ assets/elm/DrinkCard.elm
@@ -1,8 +1,8 @@
-module DrinkCard exposing (..)
+module DrinkCard exposing (drinkCard)
 
-import SharedTypes exposing (Drink)
 import Html exposing (..)
 import Html.Attributes exposing (..)
+import SharedTypes exposing (Drink)
 
 
 drinkCard : Int -> Drink -> Html msg
@@ -23,7 +23,7 @@ drinkCard index d =
                 [ div [ class "bb b--pink mt2 mh2 pb3 center" ]
                     [ h4 [ class "f4 lh4 mb1" ] [ text d.name ]
                     , p [ class "f5 lh5 mv1" ] [ text "by" ]
-                    , a [ class "f4 lh4 cs-mid-blue mv1", href <| "/brands/" ++ d.brand ] [ text d.brand ]
+                    , a [ class "f4 lh4 cs-mid-blue mv1", href <| brandNameURL d ] [ text d.brand ] -- this line. d.brand link
                     ]
                 , div [ class "flex flex-wrap" ]
                     [ p [ class "w-50 pv2 dib" ]
@@ -52,3 +52,8 @@ drinkCard index d =
                 ]
             ]
         ]
+
+
+brandNameURL : Drink -> String
+brandNameURL drink =
+    "/brands/" ++ drink.brand |> String.split " " |> String.join "-"
