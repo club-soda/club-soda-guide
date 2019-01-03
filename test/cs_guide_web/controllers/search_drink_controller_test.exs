fd0f9713f4ef825f6ad88f4caf45d5a1bc126d8c
SimonLab <simon.labondance@gmail.com>
2019-01-03 12:35:49 +0000

    add test for searching with query parameter, #290

---
+++ test/cs_guide_web/controllers/search_drink_controller_test.exs
@@ -0,0 +1,69 @@
+defmodule CsGuideWeb.SearchDrinkControllerTest do
+  use CsGuideWeb.ConnCase
+  import CsGuide.SetupHelpers
+  alias CsGuide.{Resources, Categories, Fixtures}
+
+  @create_types Fixtures.create_types()
+  @create_brand Fixtures.create_brand()
+
+  @create_attrs %{
+    name: "AF Beer 1",
+    abv: "0.5",
+    description: "Description of drink",
+    weighting: 1,
+    drink_types: %{"Beer" => "on"}
+  }
+
+  describe "Search drink" do
+    setup [:drink_setup]
+
+    test "GET /search/drinks with term", %{conn: conn} do
+      conn = get(conn, "/search/drinks?term=beer")
+      assert html_response(conn, 200) =~ "AF Beer 1"
+    end
+  end
+
+  def fixture(:type) do
+    types =
+      @create_types
+      |> Enum.map(fn t ->
+        {:ok, type} =
+          %Categories.DrinkType{}
+          |> Categories.DrinkType.changeset(t)
+          |> Categories.DrinkType.insert()
+
+        type
+      end)
+
+    types
+  end
+
+  def fixture(:brand) do
+    {:ok, brand} =
+      %Resources.Brand{}
+      |> Resources.Brand.changeset(@create_brand)
+      |> Resources.Brand.insert()
+
+    brand
+  end
+
+  def fixture(:drink, brand) do
+    {:ok, drink} =
+      @create_attrs
+      |> Map.put(:brand, brand)
+      |> Resources.Drink.insert()
+
+    drink
+  end
+
+  defp drink_setup(_) do
+    brand = fixture(:brand)
+    {:ok, brand: brand}
+
+    type = fixture(:type)
+    {:ok, type: type}
+
+    drink = fixture(:drink, brand.name)
+    {:ok, drink: drink}
+  end
+end
