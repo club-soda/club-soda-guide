d81330cc163da568647169d9d039c9e8b347e1e3
SimonLab <simon.labondance@gmail.com>
2019-01-03 12:02:33 +0000

    let phoenix parse query parameters, #290

--- lib/cs_guide_web/controllers/search_drink_controller.ex
+++ lib/cs_guide_web/controllers/search_drink_controller.ex
@@ -3,14 +3,16 @@ defmodule CsGuideWeb.SearchDrinkController do
 
   alias CsGuide.Resources.Drink
 
-  def index(conn, _params) do
+  def index(conn, params) do
     drinks = Drink.all()
     |> Drink.preload([:brand, :drink_types, :drink_styles, :drink_images])
     |> Enum.sort_by(fn d -> Map.get(d, :weighting, 0) end, &>=/2)
 
     drink_cards = Enum.map(drinks, fn d -> Drink.get_drink_card(d) end)
+    term = params["term"] || ""
+    drink_type = (params["drink_type"] && Regex.replace(~r/_/,params["drink_type"], " ")) || "none";
 
-    render(conn, "index.html", drinks: drink_cards)
+    render(conn, "index.html", drinks: drink_cards, term: term, drink_type: drink_type)
   end
 
 end
