9b7fe88ec0a0d4fa016a3755c22d0e0039f8649e
Cleop <cleo.pearson@hotmail.co.uk>
2019-01-21 10:11:10 +0000

    #344 Ensures err msg only changes on the specified field type

--- lib/cs_guide/changeset_helpers.ex
+++ lib/cs_guide/changeset_helpers.ex
@@ -1,11 +1,11 @@
 defmodule CsGuide.ChangesetHelpers do
-  def update_error_msg(changeset, type, error_msg) do
+  def update_error_msg(changeset, field_type, error_msg) do
     Map.update!(
       changeset,
       :errors,
       &Enum.map(&1, fn {key, {msg, types}} = error ->
         case types do
-          [type: type, validation: :cast] ->
+          [type: ^field_type, validation: :cast] ->
             {key, {error_msg, types}}
 
           _ ->
