d3cb5e2bc3f3e1322fe53039be2ef3bd6e509b9f
Robert Francis <robertefrancis18@gmail.com>
2019-03-28 17:17:10 +0000

    logs user in when creating a venue #53

--- lib/cs_guide_web/controllers/signup_controller.ex
+++ lib/cs_guide_web/controllers/signup_controller.ex
@@ -3,10 +3,13 @@ defmodule CsGuideWeb.SignupController do
 
   alias CsGuide.{Resources.Venue, Auth}
 
-  def create(conn, %{"venue" => %{"users" => _users} = venue}) do
+  def create(conn, %{"venue" => %{"users" => users} = venue}) do
     postcode = venue["postcode"]
     slug = Venue.create_slug(venue["venue_name"], postcode)
     venue_params = Map.put(venue, "slug", slug)
+
+    # capturing the email value so that we can email the user a password link
+    %{"0" => %{"email" => _email}} = users
 
     changeset =
       %Venue{}
@@ -16,13 +19,26 @@ defmodule CsGuideWeb.SignupController do
 
     case Venue.insert(changeset, venue_params) do
       {:ok, venue} ->
-        conn
-        |> put_flash(:info, "Venue created successfully.")
-        |> Auth.venue_owner(venue)
-        |> redirect(to: venue_path(conn, :show, venue.slug))
+        case conn.assigns.current_user do
+          nil ->
+            # logs the newly created user into the app
+            user = venue.users |> Enum.at(0)
+
+            conn
+            |> put_flash(:info, "Venue created successfully.")
+            |> Auth.venue_owner(venue)
+            |> Auth.login(user)
+            |> redirect(to: venue_path(conn, :show, venue.slug))
+
+          _user ->
+            # if a user is logged in then we create venue and user but do not log in new user
+            conn
+            |> put_flash(:info, "Venue created successfully.")
+            |> Auth.venue_owner(venue)
+            |> redirect(to: venue_path(conn, :show, venue.slug))
+        end
 
       {:error, %Ecto.Changeset{} = changeset} ->
-
         conn
         |> put_view(CsGuideWeb.UserView)
         |> render(:new,
