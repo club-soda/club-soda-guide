49895721eb22b7936335fbd9d2d780935e626248
Robert Francis <robertefrancis18@gmail.com>
2019-04-01 17:30:32 +0100

    create function to check if venue admin is allowed to edit venue in question #53

--- lib/cs_guide_web/plugs/auth.ex
+++ lib/cs_guide_web/plugs/auth.ex
@@ -1,13 +1,16 @@
 defmodule CsGuideWeb.Plugs.Auth do
   import Plug.Conn
+  import Ecto.Query, only: [from: 2]
 
   alias CsGuide.Accounts.User
+  alias CsGuide.Resources.Venue
 
   def init(default), do: default
 
   def call(conn, _params) do
     user_id = get_session(conn, :user_id)
     user = user_id && is_binary(user_id) && User.get(user_id)
+    user = user_id && add_editable_venues(user)
     assign(conn, :current_user, user)
   end
 
@@ -16,27 +19,33 @@ defmodule CsGuideWeb.Plugs.Auth do
   end
 
   def authenticate_venue_owner(conn, _opts) do
-    authenticate_user_type(conn, [:venue_admin, :site_admin])
+    authenticate_user_type(conn, [:site_admin, :venue_admin])
   end
 
   defp authenticate_user_type(conn, user_types) do
     user = conn.assigns.current_user
-    cond do
-      user && Enum.any?(user_types, &(&1 == user.role)) ->
-        conn
+    if user && Enum.any?(user_types, &(&1 == user.role)) do
+      conn
+    else
+      conn
+      |> Phoenix.Controller.redirect(to: CsGuideWeb.Router.Helpers.session_path(conn, :new))
+      |> halt()
+    end
+  end
 
-      user == nil ->
-        conn
-        # Not sure what this line is for. I think that it can be removed
-        # |> Plug.Conn.put_session(:redirect_url, conn.request_path)
-        |> Phoenix.Controller.put_flash(:error, "You must be logged in to access that page")
-        |> Phoenix.Controller.redirect(to: CsGuideWeb.Router.Helpers.session_path(conn, :new))
-        |> halt()
+  defp add_editable_venues(user) do
+    if user.role == :venue_admin do
+      query =
+        from(v in Venue,
+        join: vu in "venues_users",
+        on: v.id == vu.venue_id,
+        where: vu.user_id == ^user.id,
+        select: v.entry_id)
 
-      true ->
-        conn
-        |> Phoenix.Controller.redirect(to: "/")
-        |> halt()
+      current_users_venues = CsGuide.Repo.all(query)
+      Map.put(user, :current_users_venues, current_users_venues)
+    else
+      user
     end
   end
 end
