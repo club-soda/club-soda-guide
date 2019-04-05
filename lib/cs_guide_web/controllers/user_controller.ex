f01c6986e0e5ed0f162e06b8c29198773571812b
Robert Francis <robertefrancis18@gmail.com>
2019-04-05 12:28:39 +0100

    create routes to allow site admin to make new site admins #66

--- lib/cs_guide_web/controllers/user_controller.ex
+++ lib/cs_guide_web/controllers/user_controller.ex
@@ -4,6 +4,8 @@ defmodule CsGuideWeb.UserController do
   alias CsGuide.Accounts.User
   alias CsGuide.Resources.Venue
 
+  @mailer Application.get_env(:cs_guide, :mailer) || CsGuide.Mailer
+
   def index(conn, _params) do
     users = User.all()
     render(conn, "index.html", users: users)
@@ -55,6 +57,49 @@ defmodule CsGuideWeb.UserController do
       {:error, %Ecto.Changeset{} = changeset} ->
         render(conn, "edit.html", user: user, changeset: changeset)
     end
+  end
+
+  def new_site_admin(conn, _params) do
+    changeset = User.changeset(%User{}, %{})
+    render(conn, "new_site_admin.html", changeset: changeset)
+  end
+
+  def create_site_admin(conn, %{"user" => params}) do
+    params = Map.put(params, "role", "site_admin")
+    changeset = %User{} |> User.changeset(params)
+
+    case User.insert(changeset) do
+      {:ok, user} ->
+        subject = "Club Soda site admin"
+        one_day = 86400 # one day in seconds
+        user = User.reset_password_token(user, one_day * 10)
+
+        CsGuide.Email.send_email(user.email, subject, get_message(user))
+        |> @mailer.deliver_now()
+
+        conn
+        |> put_flash(:info, "Site Amdin created successfully. User will receive an email with instructions on setting a password")
+        |> redirect(to: admin_path(conn, :index))
+
+      {:error, %Ecto.Changeset{} = changeset} ->
+        changeset =
+          case changeset.errors do
+            [email_hash: {error, _}] ->
+              Ecto.Changeset.add_error(changeset, :email, error)
+            _ ->
+              changeset
+          end
+
+        render(conn, "new_site_admin.html", changeset: changeset)
+    end
+  end
+
+  defp get_message(user) do
+    """
+    You have been made a site admin.
+    Please click the following link to set the password on your account.
+    #{Application.get_env(:cs_guide, :site_url)}/password/#{user.password_reset_token}/edit
+    """
   end
 
   # def delete(conn, %{"id" => id}) do
