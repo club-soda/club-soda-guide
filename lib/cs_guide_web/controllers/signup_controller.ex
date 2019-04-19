4ebe126fd8a28563874677e0d12d55822cf0dc36
Robert Francis <robertefrancis18@gmail.com>
2019-04-19 13:28:46 +0100

    updates signup controller so that it no longer allows duplicate users to be created #82

--- lib/cs_guide_web/controllers/signup_controller.ex
+++ lib/cs_guide_web/controllers/signup_controller.ex
@@ -1,50 +1,72 @@
 defmodule CsGuideWeb.SignupController do
   use CsGuideWeb, :controller
 
-  alias CsGuide.Accounts.User
+  alias CsGuide.Accounts.{User, VenueUser}
   alias CsGuide.Resources.Venue
-  alias CsGuide.{Auth, Mailer}
+  alias CsGuide.{Auth, Mailer, Repo}
 
   @mailer Application.get_env(:cs_guide, :mailer) || Mailer
   @one_day 86400 # number of seconds in one day
   @five_days @one_day * 5
 
-  def create(conn, %{"venue" => venue_params}) do
+  # create for users who are not logged in
+  def create(conn, %{"venue" => %{"users" => users} = venue_params}) do
+    %{"0" => %{"email" => email}} = users
     venue_params = put_in(venue_params, ["users", "0", "role"], "venue_admin")
     changeset = Venue.changeset(%Venue{}, venue_params)
 
-    case Venue.insert(changeset, venue_params) do
-      {:ok, venue} ->
-        user =
-          venue.users
-          |> Enum.at(0)
-          |> User.reset_password_token(@five_days)
+    with(
+      true <- changeset.valid?,
+      nil <- User.get_by(email_hash: email),
+      {:ok, venue} <- Venue.insert(changeset, venue_params)
+    ) do
+      user =
+        venue.users
+        |> Enum.at(0)
+        |> User.reset_password_token(@five_days)
+        |> User.verify_user()
 
-        send_email(user)
+      send_email(user)
 
-        case conn.assigns.current_user do
-          nil ->
-            # logs the newly created user into the app
-            conn
-            |> put_flash(:info, "Venue created successfully. Email has been sent for user to confirm account")
-            |> Auth.login(user)
-            |> redirect(to: venue_path(conn, :show, venue.slug))
+      conn
+      |> put_flash(:info, "Venue created successfully. Email has been sent to allow you to confirm your account")
+      |> Auth.login(user)
+      |> redirect(to: venue_path(conn, :show, venue.slug))
 
-          _user ->
-            # if a user is logged in then we create venue and user but do not log in new user
-            conn
-            |> put_flash(:info, "Venue created successfully.")
-            |> redirect(to: venue_path(conn, :show, venue.slug))
-        end
+    else
+      # If email address already belongs to a user
+      %User{} ->
+        conn
+        |> put_flash(:error, "Email address already in use.")
+        |> put_view(CsGuideWeb.UserView)
+        |> render("new.html", changeset: changeset)
 
-      {:error, %Ecto.Changeset{} = changeset} ->
+      # if changeset is invalid or insert fails
+      _ ->
         conn
         |> put_view(CsGuideWeb.UserView)
-        |> render(:new,
-          user_changeset: changeset,
-          venue_changeset: changeset,
-          changeset: changeset
-        )
+        |> render("new.html", changeset: changeset)
+    end
+  end
+
+  # create for logged in users
+  def create(conn, %{"venue" => venue_params}) do
+    changeset = Venue.changeset(%Venue{}, venue_params)
+    user = conn.assigns.current_user
+
+    case Venue.insert(changeset, venue_params) do
+      {:ok, venue} ->
+        # associate the user with the venue (add them to venues_users table)
+        Repo.insert!(%VenueUser{venue_id: venue.id, user_id: user.id})
+
+        conn
+        |> put_flash(:info, "Venue created successfully")
+        |> redirect(to: venue_path(conn, :show, venue.slug))
+
+      {:error, changeset} ->
+        conn
+        |> put_view(CsGuideWeb.UserView)
+        |> render("new.html", changeset: changeset)
     end
   end
 
