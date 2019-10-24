8f748c07773ede7465eb3e461624b383a5c1e12f
SimonLab <simon.labondance@gmail.com>
2019-10-24 13:59:04 +0100

    set cover_image for brand map, https://github.com/club-soda/club-soda-guide/issues/593#issuecomment-545856934-n

--- lib/cs_guide_web/controllers/search_brand_controller.ex
+++ lib/cs_guide_web/controllers/search_brand_controller.ex
@@ -8,6 +8,7 @@ defmodule CsGuideWeb.SearchBrandController do
       Brand.all()
       |> Brand.preload(brand_images: [])
       |> Enum.sort_by(&String.first(&1.name))
+      |> Enum.map(&(Map.put(&1, :cover_image, get_cover_brand_image(&1.brand_images))))
       |> Enum.group_by(& &1.member)
 
     {members, non_members} =
@@ -19,5 +20,18 @@ defmodule CsGuideWeb.SearchBrandController do
       end
 
     render(conn, "index.html", member_brands: members, brands: non_members)
+  end
+
+  @doc """
+  Retreive the cover image for the brand.
+  If no cover image found returns nil
+  """
+  def get_cover_brand_image([]), do: nil
+
+  def get_cover_brand_image(images) do
+    images
+    |> Enum.filter(&(&1.one and not &1.deleted))
+    |> Enum.sort_by(&(&1.id), &>=/2)
+    |> Enum.at(0)
   end
 end
